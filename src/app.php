e5db8623136508c0a8bd83953de7367c954e3e85
Benjamin Doherty <bjd@bangpound.org>
2014-03-29 10:45:43 -0500

    druplex skeleton.

--- src/app.php
+++ src/app.php
@@ -6,15 +6,18 @@ use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 
-$app = new Application();
+$app = new Application(array(
+    'web_dir' => realpath(__DIR__ .'/../web/'),
+));
 $app->register(new UrlGeneratorServiceProvider());
 $app->register(new ValidatorServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new TwigServiceProvider());
-$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
-    // add custom globals, filters, tags, ...
 
-    return $twig;
-}));
+$app->register(new \Bangpound\Silex\LegacyPhpHttpKernelProvider());
+$app->register(new \Bangpound\Silex\DrupalServiceProvider());
+$app['legacy.request_matcher'] = $app->share(function ($c) {
+    return $c['drupal.request_matcher'];
+});
 
 return $app;
