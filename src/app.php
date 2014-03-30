a24fe64f20473d782c1fac98ffd986976580e1f1
Benjamin Doherty <bjd@bangpound.org>
2014-03-30 17:27:55 -0500

    import classes.

--- src/app.php
+++ src/app.php
@@ -2,6 +2,8 @@
 
 use Silex\Application;
 use Bangpound\Bridge\Drupal\Controller\ControllerResolver;
+use Bangpound\Silex\DrupalServiceProvider;
+use Bangpound\Silex\LegacyPhpHttpKernelProvider;
 
 $app = new Application(array(
     'web_dir' => realpath(__DIR__ .'/../web/'),
@@ -10,8 +12,8 @@ $app['resolver'] = $app->share($app->extend('resolver', function ($resolver, $c)
     return new ControllerResolver($resolver, $c['legacy.request_matcher']);
 }));
 
-$app->register(new \Bangpound\Silex\LegacyPhpHttpKernelProvider());
-$app->register(new \Bangpound\Silex\DrupalServiceProvider());
+$app->register(new LegacyPhpHttpKernelProvider());
+$app->register(new DrupalServiceProvider());
 $app['legacy.request_matcher'] = $app->share(function ($c) {
     return $c['drupal.request_matcher'];
 });
