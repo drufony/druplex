718b8f5aea21a8a2def8bbe9faad6fcb26164662
Benjamin Doherty <bjd@bangpound.org>
2014-03-30 17:26:27 -0500

    move web profiler dependencies into dev environment.

--- config/dev.php
+++ config/dev.php
@@ -2,12 +2,21 @@
 
 use Silex\Provider\MonologServiceProvider;
 use Silex\Provider\WebProfilerServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\UrlGeneratorServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+
+// enable the debug mode
+$app['debug'] = true;
 
 // include the prod configuration
 require __DIR__.'/prod.php';
 
-// enable the debug mode
-$app['debug'] = true;
+$app->register(new UrlGeneratorServiceProvider());
+$app->register(new ValidatorServiceProvider());
+$app->register(new ServiceControllerServiceProvider());
+$app->register(new TwigServiceProvider());
 
 $app->register(new MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__.'/../var/logs/silex_dev.log',
--- src/app.php
+++ src/app.php
@@ -1,18 +1,10 @@
 <?php
 
 use Silex\Application;
-use Silex\Provider\TwigServiceProvider;
-use Silex\Provider\UrlGeneratorServiceProvider;
-use Silex\Provider\ValidatorServiceProvider;
-use Silex\Provider\ServiceControllerServiceProvider;
 
 $app = new Application(array(
     'web_dir' => realpath(__DIR__ .'/../web/'),
 ));
-$app->register(new UrlGeneratorServiceProvider());
-$app->register(new ValidatorServiceProvider());
-$app->register(new ServiceControllerServiceProvider());
-$app->register(new TwigServiceProvider());
 
 $app->register(new \Bangpound\Silex\LegacyPhpHttpKernelProvider());
 $app->register(new \Bangpound\Silex\DrupalServiceProvider());
