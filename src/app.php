8982b3fb0b2ab76dced000974a2783732b169183
Benjamin Doherty <bjd@bangpound.org>
2014-03-30 17:27:39 -0500

    add drupal controller resolver.

--- src/app.php
+++ src/app.php
@@ -1,10 +1,14 @@
 <?php
 
 use Silex\Application;
+use Bangpound\Bridge\Drupal\Controller\ControllerResolver;
 
 $app = new Application(array(
     'web_dir' => realpath(__DIR__ .'/../web/'),
 ));
+$app['resolver'] = $app->share($app->extend('resolver', function ($resolver, $c) {
+    return new ControllerResolver($resolver, $c['legacy.request_matcher']);
+}));
 
 $app->register(new \Bangpound\Silex\LegacyPhpHttpKernelProvider());
 $app->register(new \Bangpound\Silex\DrupalServiceProvider());
