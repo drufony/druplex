204404889f7bce14cd13b238f63bab620e9952bb
Benjamin Doherty <bjd@bangpound.org>
2014-03-31 00:19:49 -0500

    remove controller resolver and request matcher service from main app configuration.

--- src/app.php
+++ src/app.php
@@ -9,15 +9,8 @@ use Silex\Provider\TwigServiceProvider;
 $app = new Application(array(
     'web_dir' => realpath(__DIR__ .'/../web/'),
 ));
-$app['resolver'] = $app->share($app->extend('resolver', function ($resolver, $c) {
-    return new ControllerResolver($resolver, $c['legacy.request_matcher']);
-}));
-
 $app->register(new TwigServiceProvider());
 $app->register(new LegacyPhpHttpKernelProvider());
 $app->register(new DrupalServiceProvider());
-$app['legacy.request_matcher'] = $app->share(function ($c) {
-    return $c['drupal.request_matcher'];
-});
 
 return $app;
